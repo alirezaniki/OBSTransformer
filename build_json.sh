137bc8e061018422f645e3a5b61ac1debdec2c76
Alireza Niksejel <alireza.niksejel@dal.ca>
2023-05-18 18:01:48 -0300

    Add files via upload

---
+++ build_json.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# May 18 2023
+# Alrieza Niksejel (alireza.niksejel@dal.ca)
+
+# provides the required station_list.json file
+# station info file needs to follow below format:
+# longitude(deg) latiude(deg) network station channel depth(km)
+
+
+
+# full path to station info file
+data_dir="/media/dalquake/T7/3k_OBS_validation_dataset/Github/dataset"	
+# full path to dataset		
+station_info="/media/dalquake/T7/3k_OBS_validation_dataset/Github/stations.dat"	
+
+
+
+[[ ! -d $data_dir ]] || [[ ! -f $station_info ]] && echo "requirement(s) missing" && exit
+[[ -d json ]] && rm -r json/* 2> /dev/null || mkdir json
+
+cd $data_dir
+n=`grep -c '^[1-9]' $station_info`
+i=1
+for sta in `find ./ -type f -name "*mseed" | cut -f 3 -d / | cut -f 2 -d . | sort | uniq`; do
+	net=`grep "$sta" $station_info | awk '{print $3}' | head -1`
+	cha=`grep "$sta" $station_info | awk '{print $5}' | head -1`
+	lat=`grep "$sta" $station_info | awk '{print $2}' | head -1`
+	lon=`grep "$sta" $station_info | awk '{print $1}' | head -1`
+	dep=`grep "$sta" $station_info | awk '{print $6}' | head -1`
+	if [[ $i = 1 ]]; then
+		echo "{\"$sta\": {\"network\": \"$net\", \"channels\": [\""$cha"1\", \""$cha"2\", \""$cha"Z\"], \"coords\": [$lat, $lon, $dep]}," >> ../json/station_list.json
+	elif [[ $i = $n ]]; then
+		echo "\"$sta\": {\"network\": \"$net\", \"channels\": [\""$cha"1\", \""$cha"2\", \""$cha"Z\"], \"coords\": [$lat, $lon, $dep]}}" >> ../json/station_list.json
+	else
+		echo "\"$sta\": {\"network\": \"$net\", \"channels\": [\""$cha"1\", \""$cha"2\", \""$cha"Z\"], \"coords\": [$lat, $lon, $dep]}," >> ../json/station_list.json
+	fi
+	echo "done with station $sta"
+	i=$((i+1))
+done
---
+++ detection.py
@@ -0,0 +1,32 @@
+import os, sys
+from EQTransformer.utils.hdf5_maker import preprocessor
+
+
+input = sys.argv[1].replace('/', '')
+
+
+json_basepath = os.path.join(os.getcwd(),"json/station_list.json")
+if not os.path.isdir(f'./{input}_processed_hdfs'):
+    preprocessor(preproc_dir="./preproc",
+                mseed_dir=input,
+                stations_json=json_basepath, 
+                overlap=0.0,
+                n_processor=10)
+
+from EQTransformer.core.predictor import predictor
+predictor(input_dir=f"./{input}_processed_hdfs",
+         input_model="./models/OBSTransformer/OBSTransformer.h5",
+         output_dir="output/OBSTransformer",
+          estimate_uncertainty=False,
+          output_probabilities=False,
+          number_of_sampling=5,
+          loss_weights=[0.02, 0.40, 0.58],
+          detection_threshold=0.3,                
+          P_threshold=0.1,
+          S_threshold=0.1, 
+          number_of_plots=50,
+          plot_mode='time',
+          batch_size=500,
+          number_of_cpus=10,
+          keepPS=False,
+          spLimit=60)
