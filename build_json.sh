815f990961424a83d4eb32338b6c3b59963961bf
Alireza Niksejel <alireza.niksejel@dal.ca>
2023-06-26 15:04:06 -0300

    Update build_json.sh

--- build_json.sh
+++ build_json.sh
@@ -8,14 +8,13 @@
 
 
 
-
 # Full path to the dataset
 data_dir="/media/dalquake/T7/3k_OBS_validation_dataset/Github/dataset"	
 # Full path to the station info file		
 station_info="/media/dalquake/T7/3k_OBS_validation_dataset/Github/stations.dat"	
 
 
-
+pickhome=`pwd`
 [[ ! -d $data_dir ]] || [[ ! -f $station_info ]] && echo "requirement(s) missing" && exit
 [[ -d json ]] && rm -r json/* 2> /dev/null || mkdir json
 
@@ -29,11 +28,11 @@ for sta in `find ./ -type f -name "*mseed" | cut -f 3 -d / | cut -f 2 -d . | sor
 	lon=`grep "$sta" $station_info | awk '{print $1}' | head -1`
 	dep=`grep "$sta" $station_info | awk '{print $6}' | head -1`
 	if [[ $i = 1 ]]; then
-		echo "{\"$sta\": {\"network\": \"$net\", \"channels\": [\""$cha"1\", \""$cha"2\", \""$cha"Z\"], \"coords\": [$lat, $lon, $dep]}," >> ../json/station_list.json
+		echo "{\"$sta\": {\"network\": \"$net\", \"channels\": [\""$cha"1\", \""$cha"2\", \""$cha"Z\"], \"coords\": [$lat, $lon, $dep]}," >> $pickhome/json/station_list.json
 	elif [[ $i = $n ]]; then
-		echo "\"$sta\": {\"network\": \"$net\", \"channels\": [\""$cha"1\", \""$cha"2\", \""$cha"Z\"], \"coords\": [$lat, $lon, $dep]}}" >> ../json/station_list.json
+		echo "\"$sta\": {\"network\": \"$net\", \"channels\": [\""$cha"1\", \""$cha"2\", \""$cha"Z\"], \"coords\": [$lat, $lon, $dep]}}" >> $pickhome/json/station_list.json
 	else
-		echo "\"$sta\": {\"network\": \"$net\", \"channels\": [\""$cha"1\", \""$cha"2\", \""$cha"Z\"], \"coords\": [$lat, $lon, $dep]}," >> ../json/station_list.json
+		echo "\"$sta\": {\"network\": \"$net\", \"channels\": [\""$cha"1\", \""$cha"2\", \""$cha"Z\"], \"coords\": [$lat, $lon, $dep]}," >> $pickhome/json/station_list.json
 	fi
 	echo "done with station $sta"
 	i=$((i+1))
